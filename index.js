2b96a244663eb1aedde90ebc3da5744f12659694
Mark Nguyen <mark.t.nguyen@outlook.com>
2025-01-28 16:37:16 -0600

    Refactored conversion functions to a dictionary combined with constant variables to enhance readability

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 /*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
+1 meter = 3.28084 feet
+1 liter = 0.264172 gallon
+1 kilogram = 2.20462 pound
+
+Length (Meter/Feet)
+20 meters = 65.616 feet | 20 feet = 6.096 meters
+
+Volume (Liters/Gallons)
+20 liters = 5.284 gallons | 20 gallons = 75.708 liters
+
+Mass (Kilograms/Pounds)
+20 kilos = 44.092 pounds | 20 pounds = 9.072 kilos
+
 */
+
+const METERS_TO_FEET = 3.28084
+const LITERS_TO_GALLONS = 0.264172
+const KILOS_TO_POUNDS = 2.20462
+
+const conversion = {
+    "meters": (meters) => {return (meters * METERS_TO_FEET).toFixed(3)},
+    "liters": (liters) => {return (liters * LITERS_TO_GALLONS).toFixed(3)},
+    "kilos": (kilos) => {return (kilos * KILOS_TO_POUNDS).toFixed(3)}
+}
 
 const convertBtn = document.querySelector("#convert-btn")
 const inputTxt = document.querySelector("#input-txt")
@@ -16,6 +36,8 @@ convertBtn.addEventListener("click", () => {
 
 })
 
+console.log(conversion["meters"](20))
+
 // Let's aim for DRY
 
 // function render(element) {
@@ -28,16 +50,4 @@ convertBtn.addEventListener("click", () => {
 
 function renderHTML(elementId, inputValue, result) {
     const el = document.querySelector(`#${elementId}`)
-}
-
-function calcMetersToFeet(meters) {
-    return (meters * 3.281).toFixed(3)
-}
-
-function calcLitersToGallons(liters) {
-    return (liters * 0.264).toFixed(3)
-}
-
-function calcKilosToPounds(kilos) {
-    return (kilos * 2.204).toFixed(3)
 }
